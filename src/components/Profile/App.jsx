a753994631293a07fd893b054c6b0fc56bac5f1e
TetianaKuzma <tetiana.b97@gmail.com>
2023-01-12 20:29:37 +0200

    Add Profile

---
+++ src/components/Profile/App.jsx
@@ -0,0 +1,20 @@
+import Profile from './Profile/Profile';
+import user from 'db/user.json';
+
+const App = () => {
+  return (
+    <>
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </>
+  );
+};
+
+export default App;
