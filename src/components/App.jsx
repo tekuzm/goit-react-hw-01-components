96e325330abd81b67ce3b205960021ece6939cac
TetianaKuzma <tetiana.b97@gmail.com>
2023-01-13 23:33:14 +0200

    Add task 3, add hover states

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,11 @@
 import Profile from './Profile/Profile';
 import Statistics from './Statistics/Statistics';
 import FriendList from './FriendList/FriendList';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
 import user from 'db/user.json';
 import data from 'db/data.json';
 import friends from 'db/friends.json';
+import transactions from 'db/transactions.json';
 
 const App = () => {
   return (
@@ -19,6 +21,7 @@ const App = () => {
       />
       <Statistics title="Upload stats" stats={data} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
